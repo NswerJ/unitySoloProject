c8918ebb1b42f005a25ff0c280d4501cefe3bebd
Wizone06 <haemjjiyul548@gmail.com>
2023-04-21 23:28:37 +0900

    ehfflrl
    돌리기

--- UnitySoloProject/Assets/01.Script/UIManager.cs
+++ UnitySoloProject/Assets/01.Script/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,35 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private float _turnSpeed = 10f;
     public Transform _cameraTrm;
+    private float _currentYRotR = 0f;
+    private float _currentYRotL;
 
-    private float _currentYRot = 0f;
+    private float saveYRotL;
+    private float time;
 
     public void RRotationChange()
     {
-        _currentYRot = Mathf.Clamp(_currentYRot + 90f, -90f, 90f);
-        _cameraTrm.transform.rotation = Quaternion.Euler(0, _currentYRot, 0);
+        _currentYRotR = Mathf.Clamp(_currentYRotR + 90f, -90f, 90f);
+        //_cameraTrm.transform.rotation = Quaternion.Euler(0, _currentYRot, 0);
     }
     public void LRotationChange()
     {
-        _currentYRot = Mathf.Clamp(_currentYRot - 90f, -90f, 90f);
-        _cameraTrm.transform.rotation = Quaternion.Euler(0, _currentYRot, 0);
+        _currentYRotL = Mathf.Clamp(_currentYRotL - 90f, -90f, 90f);
+        time = 0;
+        saveYRotL = transform.rotation.y;
+        StartCoroutine(lRotation());
+        //_cameraTrm.transform.rotation = Quaternion.Euler(0, _currentYRot, 0);
+    }
+
+    private IEnumerator lRotation()
+    {
+        Vector3 rot = transform.rotation.eulerAngles;
+        while (rot.y != _currentYRotL)
+        {
+            time += Time.deltaTime;
+            rot.y = Mathf.Lerp(saveYRotL, _currentYRotL, time);
+            transform.rotation = Quaternion.Euler(0,rot.y,0);
+            yield return null;
+        }
     }
 }
