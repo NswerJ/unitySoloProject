c27c9e715b58a9b1b24bc422b19d195940dd9fcc
Wizone06 <haemjjiyul548@gmail.com>
2023-04-22 23:12:44 +0900

    돌리기 오류 1
    돌리기 오류 1

--- UnitySoloProject/Assets/01.Script/UIManager.cs
+++ UnitySoloProject/Assets/01.Script/UIManager.cs
@@ -8,36 +8,41 @@ public class UIManager : MonoBehaviour
 
     [SerializeField]
     private float _turnSpeed = 10f;
-    public Transform _cameraTrm;
-    private float _currentYRotR = 0f;
-    private float _currentYRotL;
 
+    public Transform _cameraTrm;
+
+    public bool _isRotChange = true;
+
+    private float _currentYRotR = 0f;
+    private float _currentYRotL = 0f;
+    
     private float saveYRotL;
-    private float time;
 
     public void RRotationChange()
     {
         _currentYRotR = Mathf.Clamp(_currentYRotR + 90f, -90f, 90f);
-        //_cameraTrm.transform.rotation = Quaternion.Euler(0, _currentYRot, 0);
-    }
-    public void LRotationChange()
-    {
-        _currentYRotL = Mathf.Clamp(_currentYRotL - 90f, -90f, 90f);
-        time = 0;
-        saveYRotL = transform.rotation.y;
-        StartCoroutine(lRotation());
-        //_cameraTrm.transform.rotation = Quaternion.Euler(0, _currentYRot, 0);
+        StartCoroutine(RotationChange(_currentYRotR));
     }
 
-    private IEnumerator lRotation()
+    public void LRotationChange()
     {
-        Vector3 rot = transform.rotation.eulerAngles;
-        while (rot.y != _currentYRotL)
+        
+        _currentYRotL = Mathf.Clamp(_currentYRotL - 90f, -90f, 90f);
+        StartCoroutine(RotationChange(_currentYRotL));
+    }
+
+    private IEnumerator RotationChange(float targetYRot)
+    {
+        float startYRot = _cameraTrm.localRotation.eulerAngles.y;
+        float elapsedTime = 0;
+
+        while (elapsedTime < _turnSpeed)
         {
-            time += Time.deltaTime;
-            rot.y = Mathf.Lerp(saveYRotL, _currentYRotL, time);
-            transform.rotation = Quaternion.Euler(0,rot.y,0);
+            elapsedTime += Time.deltaTime;
+            float yRot = Mathf.Lerp(startYRot, targetYRot, elapsedTime / _turnSpeed);
+            _cameraTrm.localRotation = Quaternion.Euler(0, yRot, 0);
             yield return null;
         }
+        _cameraTrm.localRotation = Quaternion.Euler(0, targetYRot, 0);
     }
 }
