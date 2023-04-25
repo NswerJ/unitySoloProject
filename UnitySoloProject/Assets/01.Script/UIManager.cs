fec247f5d567933f08586a4c07b088e7e76e0a39
Wizone06 <haemjjiyul548@gmail.com>
2023-04-26 00:48:13 +0900

    화면돌릴때 라이트 키고 끄기
    화면돌릴때 라이트 키고 끄기

--- UnitySoloProject/Assets/01.Script/UIManager.cs
+++ UnitySoloProject/Assets/01.Script/UIManager.cs
@@ -8,13 +8,19 @@ public class UIManager : MonoBehaviour
 
     [SerializeField]
     private float _turnSpeed = 10f;
-
+    [SerializeField]
+    private Light _light;
     public Transform _cameraTrm;
 
     // 시작 로테이션 값을 현재 로테이션 값으로 초기화합니다.
     private Vector3 _currentRotation = Vector3.zero;
 
     private float saveYRotL;
+
+    private void Awake()
+    {
+        _light = transform.Find("LightHandle").GetComponent<Light>(); 
+    }
 
     public void RRotationChange()
     {
@@ -35,13 +41,24 @@ public class UIManager : MonoBehaviour
             StartCoroutine(RotationChange(targetYRot));
         }
     }
+    private void Update()
+    {
+        if (_currentRotation.y == 90f)
+        {
+            _light.intensity = 2f;
+        }
+        else if (_currentRotation.y == 0 || _currentRotation.y == 180)
+        {
+            _light.intensity = 8f;
+        }
+    }
 
     private IEnumerator RotationChange(float targetYRot)
     {
         // 시작 로테이션 값을 현재 로테이션 값으로 변경합니다.
         Vector3 startRotation = _cameraTrm.localRotation.eulerAngles;
         float elapsedTime = 0;
-
+        _light.enabled = false;
         while (elapsedTime < _turnSpeed)
         {
             elapsedTime += Time.deltaTime;
@@ -50,6 +67,7 @@ public class UIManager : MonoBehaviour
             yield return null;
         }
 
+        _light.enabled = true;
         _cameraTrm.localRotation = Quaternion.Euler(0, targetYRot, 0);
     }
 }
