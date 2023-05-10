a9b035ccae2f657f2f7a013e7624ced77bc2da97
Wizone06 <haemjjiyul548@gmail.com>
2023-05-10 09:01:03 +0900

    setting panel
    setting panel

--- UnitySoloProject/Assets/01.Script/UI/StartUI.cs
+++ UnitySoloProject/Assets/01.Script/UI/StartUI.cs
@@ -22,6 +22,9 @@ public class StartUI : MonoBehaviour
         Button exBtn = root.Q<Button>("ExplainButton");
         Button exBtnX = root.Q<Button>("ExButtonX");
         VisualElement exPlain = root.Q("Explain");
+        Button stBtn = root.Q<Button>("SettingButton");
+        Button stBtnX = root.Q<Button>("StButtonX");
+        VisualElement setting = root.Q("Setting");
 
         startBtn.RegisterCallback<ClickEvent>(e =>
         {
@@ -34,6 +37,14 @@ public class StartUI : MonoBehaviour
         exBtnX.RegisterCallback<ClickEvent>(e =>
         {
             exPlain.RemoveFromClassList("down");
+        });
+        stBtn.RegisterCallback<ClickEvent>(s =>
+        {
+            setting.AddToClassList("down");
+        });
+        stBtnX.RegisterCallback<ClickEvent>(s =>
+        {
+            setting.RemoveFromClassList("down");
         });
     }
 }
