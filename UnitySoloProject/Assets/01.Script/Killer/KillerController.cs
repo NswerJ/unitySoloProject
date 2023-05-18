100dfcbd68674e7cc99d518c884d349d90ad58c1
Wizone06 <haemjjiyul548@gmail.com>
2023-05-18 19:35:18 +0900

    적이 플레이어한테 달려오고 달려 올때 플레이어가 적 바라보기
    적이 플레이어한테 달려오고 달려 올때 플레이어가 적 바라보기

--- UnitySoloProject/Assets/01.Script/Killer/KillerController.cs
+++ UnitySoloProject/Assets/01.Script/Killer/KillerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class KillerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float _movementSpeed = 3f; // 이동 속도
+    public float _minDistanceToPlayer = 5f; // 플레이어와의 최소 거리
+    public float _minChaseTime = 5f; // 추격 시작까지의 최소 시간
+    public float _maxChaseTime = 8f; // 추격 시작까지의 최대 시간
+
+    private Transform _playerTransform; // 플레이어의 Transform 컴포넌트
+    private bool _isChasing = false; // 추격 상태 여부
+    private float _chaseTimer; // 추격 타이머
+
+    private void Start()
     {
-        
+        _playerTransform = GameObject.FindWithTag("Player").transform;
+
+        // 추격 시작 타이머 설정
+        _chaseTimer = Random.Range(_minChaseTime, _maxChaseTime);
+        StartCoroutine(StartChasing());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_isChasing)
+        {
+            // 플레이어를 향하는 방향 계산
+            Vector3 directionToPlayer = (_playerTransform.position - transform.position).normalized;
+
+            // 플레이어를 바라보기 위한 회전
+            _playerTransform.rotation = Quaternion.LookRotation(-directionToPlayer);
+
+            // 플레이어와의 거리 계산
+            float distanceToPlayer = Vector3.Distance(transform.position, _playerTransform.position);
+
+            // 플레이어와의 거리가 최소 거리보다 크면 이동
+            if (distanceToPlayer > _minDistanceToPlayer)
+            {
+                // 이동
+                transform.Translate(directionToPlayer * _movementSpeed * Time.deltaTime, Space.World);
+            }
+        }
+    }
+
+    private IEnumerator StartChasing()
+    {
+        yield return new WaitForSeconds(_chaseTimer);
+        _isChasing = true;
     }
 }
