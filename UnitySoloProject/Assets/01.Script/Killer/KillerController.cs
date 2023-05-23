1447a8af822b0a27dc0148662768eed19493cff4
Wizone06 <haemjjiyul548@gmail.com>
2023-05-24 01:41:51 +0900

    시네머신 하려다가 포기 사운드
    시네머신 하려다가 포기 사운드

--- UnitySoloProject/Assets/01.Script/Killer/KillerController.cs
+++ UnitySoloProject/Assets/01.Script/Killer/KillerController.cs
@@ -1,27 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
 
 public class KillerController : MonoBehaviour
 {
-    public float _movementSpeed = 3f; // 이동 속도
-    public float _minDistanceToPlayer = 5f; // 플레이어와의 최소 거리
-    public float _minChaseTime = 5f; // 추격 시작까지의 최소 시간
-    public float _maxChaseTime = 8f; // 추격 시작까지의 최대 시간
+    public float _movementSpeed = 3f; 
+    public float _minDistanceToPlayer = 5f; 
+    public float _minChaseTime = 5f; 
+    public float _maxChaseTime = 8f; 
 
-    private Transform _playerTransform; // 플레이어의 Transform 컴포넌트
-    private bool _isChasing = false; // 추격 상태 여부
-    private float _chaseTimer; // 추격 타이머
+    private Transform _playerTransform; 
+    private bool _isChasing = false; 
+    private float _chaseTimer;
 
-    public CinemachineVirtualCamera VirtualCamera;
-    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+    [SerializeField]
+    private AudioSource _boomSound;
+
 
     private void Start()
     {
         _playerTransform = GameObject.FindWithTag("Player").transform;
-        virtualCameraNoise = VirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-
+        _boomSound = GetComponent<AudioSource>();
         // 추격 시작 타이머 설정
         _chaseTimer = Random.Range(_minChaseTime, _maxChaseTime);
         StartCoroutine(StartChasing());
@@ -31,30 +30,30 @@ public class KillerController : MonoBehaviour
     {
         if (_isChasing)
         {
-            // 플레이어를 향하는 방향 계산
             Vector3 directionToPlayer = (_playerTransform.position - transform.position).normalized;
 
-            // 플레이어를 바라보기 위한 회전
             _playerTransform.rotation = Quaternion.LookRotation(-directionToPlayer);
 
-            // 플레이어와의 거리 계산
             float distanceToPlayer = Vector3.Distance(transform.position, _playerTransform.position);
 
-            // 플레이어와의 거리가 최소 거리보다 크면 이동
+
             if (distanceToPlayer > _minDistanceToPlayer)
             {
-                // 이동
                 transform.Translate(directionToPlayer * _movementSpeed * Time.deltaTime, Space.World);
+                
             }
             else
             {
-                // 플레이어 스크립트의 Light 컴포넌트를 찾아서 intensity 변경
                 Light playerLight = _playerTransform.GetComponentInChildren<Light>();
+                _boomSound.Play();
                 if (playerLight != null)
                 {
                     playerLight.intensity = 2f;
                 }
+                Debug.Log("!!!!!");
+                _isChasing = false;
             }
+           
         }
     }
 
