0634363aaa1f4a0771c7b39b86ed5b9d53c2f9c4
Wizone06 <haemjjiyul548@gmail.com>
2023-06-07 00:15:21 +0900

    슬라이더 패턴 완성
    슬라이더 패턴 완성

--- UnitySoloProject/Assets/01.Script/Player/PlayerController.cs
+++ UnitySoloProject/Assets/01.Script/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,22 +15,53 @@ public class PlayerController : MonoBehaviour
     private float decreaseInterval = 0.5f; // 슬라이더가 감소하는 시간 간격
     private float timer; // 슬라이더 감소를 위한 타이머
     private Camera mainCamera;
+    private bool firstSlider = false;
+    private bool secondSliderActive = false;
+
+    RaycastHit hit;
+
+    private void Awake()
+    {
+        slider = FindObjectOfType<Slider>();
+        secondSlider = slider.transform.GetChild(0).GetComponent<Slider>();
+    }
 
     private void Start()
     {
         mainCamera = Camera.main;
         timer = decreaseInterval;
+        slider.gameObject.SetActive(false);
+        secondSlider.gameObject.SetActive(false);
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, targetLayer))
             {
-                // 특정 레이어에 맞았을 경우 첫 번째 슬라이더 값 증가
-                slider.value += 1;
+                if (!firstSlider && !secondSliderActive)
+                {
+                    firstSlider = true;
+                    slider.gameObject.SetActive(true);
+                }
+
+                if (firstSlider && !secondSliderActive)
+                {
+                    slider.value += 1;
+
+                    if (slider.value >= slider.maxValue)
+                    {
+                        slider.value = slider.maxValue;
+                        secondSlider.gameObject.SetActive(true);
+                        secondSliderActive = true;
+                    }
+                }
+
+                if (secondSliderActive)
+                {
+                    secondSlider.value += 1;
+                }
             }
         }
 
@@ -37,20 +69,31 @@ public class PlayerController : MonoBehaviour
         timer -= Time.deltaTime;
         if (timer <= 0f)
         {
-            slider.value -= decreaseAmount;
+            if (firstSlider && !secondSliderActive)
+            {
+                slider.value -= decreaseAmount;
+
+                if (slider.value <= 0)
+                {
+                    slider.value = 0;
+                    firstSlider = false;
+                    slider.gameObject.SetActive(false);
+                }
+            }
+
+            if (secondSliderActive)
+            {
+                secondSlider.value -= decreaseAmount;
+
+                if (secondSlider.value <= 0)
+                {
+                    secondSlider.value = 0;
+                    secondSlider.gameObject.SetActive(false);
+                    secondSliderActive = false;
+                }
+            }
+
             timer = decreaseInterval;
-        }
-
-        // 첫 번째 슬라이더가 최댓값에 도달하면 두 번째 슬라이더 활성화
-        if (slider.value >= slider.maxValue)
-        {
-            secondSlider.gameObject.SetActive(true);
-        }
-
-        if (secondSlider.gameObject.activeSelf)
-        {
-            // 두 번째 슬라이더의 값 증가
-            secondSlider.value += 1;
         }
     }
 }
