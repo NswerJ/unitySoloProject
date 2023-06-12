d45eb915b779746f5253ec7f796b3d734fab7058
Wizone06 <haemjjiyul548@gmail.com>
2023-06-13 00:10:31 +0900

    적
    적

--- UnitySoloProject/Assets/01.Script/Killer/KillerSpawner.cs
+++ UnitySoloProject/Assets/01.Script/Killer/KillerSpawner.cs
@@ -12,6 +12,8 @@ public class KillerSpawner : MonoBehaviour
     private Transform[] _spawnPoints; // 적 스폰 위치 배열
     public int _spawnedKillerCount; // 현재 스폰된 적 수
 
+    private bool _isSpawning; // 스폰 중인지 여부
+
     private void Start()
     {
         // 적 스폰 위치 배열 초기화
@@ -21,34 +23,59 @@ public class KillerSpawner : MonoBehaviour
             _spawnPoints[i] = _spawnPointsParent.GetChild(i);
         }
 
-        // 첫번째 적 스폰
-        StartCoroutine(SpawnEnemy());
+        // 적 스폰 시작
+        StartSpawning();
+    }
+
+    private void StartSpawning()
+    {
+        if (!_isSpawning)
+        {
+            _isSpawning = true;
+            StartCoroutine(SpawnEnemy());
+        }
+    }
+
+    private void StopSpawning()
+    {
+        if (_isSpawning)
+        {
+            _isSpawning = false;
+            StopCoroutine(SpawnEnemy());
+        }
     }
 
     private IEnumerator SpawnEnemy()
     {
-        // 적 스폰 최소/최대 시간 간격 내에서 랜덤 시간 생성
-        float spawnTime = Random.Range(_minSpawnTime, _maxSpawnTime);
-        yield return new WaitForSeconds(spawnTime);
-        // 랜덤한 위치에서 적 생성
-        Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
-        GameObject enemyObject = Instantiate(_killerPrefab, spawnPoint.position, spawnPoint.rotation);
-
-        // 적이 플레이어를 바라보도록 회전
-        Vector3 direction = (GameObject.FindWithTag("Player").transform.position - enemyObject.transform.position).normalized;
-        enemyObject.transform.LookAt(enemyObject.transform.position + direction);
-
-        // 스폰된 적 수 증가
-        _spawnedKillerCount++;
-        
-
-        // 한마리만 스폰했을 경우
-        if (_spawnedKillerCount == 1)
+        while (_isSpawning)
         {
-            yield break; // 코루틴 종료
-        }
+            // 랜덤한 시간 간격으로 스폰
+            float spawnTime = Random.Range(_minSpawnTime, _maxSpawnTime);
+            yield return new WaitForSeconds(spawnTime);
 
-        // 다음 적 스폰
-        StartCoroutine(SpawnEnemy());
+            // 랜덤한 위치에서 적 생성
+            Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+            GameObject enemyObject = Instantiate(_killerPrefab, spawnPoint.position, spawnPoint.rotation);
+
+            // 적이 플레이어를 바라보도록 회전
+            Vector3 direction = (GameObject.FindWithTag("Player").transform.position - enemyObject.transform.position).normalized;
+            enemyObject.transform.LookAt(enemyObject.transform.position + direction);
+
+            // 스폰된 적 수 증가
+            _spawnedKillerCount++;
+        }
+    }
+
+    private void Update()
+    {
+        // 한마리만 스폰했을 경우
+        if (_spawnedKillerCount >= 1)
+        {
+            StopSpawning(); // 스폰 중지
+        }
+        else if (_spawnedKillerCount == 0)
+        {
+            StartSpawning(); // 스폰 시작
+        }
     }
 }
